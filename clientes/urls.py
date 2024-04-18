5f98643df0ab30a0eb08f279cc65b8b9a73454de
JadsonC <jcss@ic.ufal.br>
2024-04-18 14:21:33 -0300

    adding clients update

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.clientes, name='clientes')
+    path('', views.clientes, name='clientes'),
+    path('atualiza_cliente/', views.att_cliente, name='atualiza_cliente')
 ]
